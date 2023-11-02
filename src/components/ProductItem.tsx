432102368ea3f4e0b841c0d1bb7b6de99cbdd6ad
Sofia Ferreira <sofia.ferreira@impactcommerce.pt>
2023-11-02 12:32:43 +0000

    change components to typescript

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/ProductItem.module.css'
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product }: { product: Product }) {
     return (
         <Link href={'../detail/' + product.id} className={styles.Product}>
             <Image src={product.image} alt={product.title}
                 width={300} height={300} style={{ width: "auto", height: "auto" }} loading="lazy" />
-            
+
             <p className='ribbon-1'>{product.category}</p>
             <h3>{product.title}</h3>
             <p className='price'>{formatPrice(product.price, 'en-US')}</p>
