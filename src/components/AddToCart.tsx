cdcc82dceacc3d0daaa5af067fffd367449f39f1
Sofia Ferreira <sofia.ferreira@impactcommerce.pt>
2023-11-14 11:13:01 +0000

    add validations to addToCart

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,16 +1,25 @@
 "use client"
 
 import useCartStore from '@/store/cartStore';
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function AddToCart({ product }) {
     const [quantity, setQuantity] = useState(1);
-    const {add: handleAddToCart} = useCartStore();
+    const {add} = useCartStore();
+
+    const handleQuantityChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const newQuantity = Number(e.target.value);
+        !isNaN(newQuantity) && setQuantity(newQuantity)
+    }, []);
+
+    const handleAddToCart = useCallback(() => {
+        add(product, quantity);
+    }, [add, product, quantity]);
 
     return (
         <div className="flex flex-row gap">
-            <input type="number" value={quantity} onChange={e => setQuantity(Number(e.target.value))} />
-            <button onClick={() => handleAddToCart(product, quantity)}>Add to cart</button>
+            <input type="number" value={quantity} onChange={handleQuantityChange} />
+            <button onClick={handleAddToCart}>Add to cart</button>
         </div>
     )
 }
